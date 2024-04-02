2f6e68194e4ee8e1f6988531e3f343516a4b9716
Zorthon <104100733+Zorthon@users.noreply.github.com>
2024-04-02 10:36:18 -0400

    lab3b

---
+++ CGrondin_Lab3B/CGrondin_Lab3B/CGrondin_Lab3B.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+int main() {
+
+
+
+	std::ifstream fIn;
+	fIn.open("C:\\Users\\008021438\\Desktop\\CGrondin_IGP\\CGrondin_Lab3B\\CGrondin_Lab3B\\Lab3b.txt");
+}
