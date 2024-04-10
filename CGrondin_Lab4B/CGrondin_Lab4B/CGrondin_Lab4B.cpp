6105aba1b0d46808af40f2ce23db74192ffee5fe
Zorthon <104100733+Zorthon@users.noreply.github.com>
2024-04-10 09:21:12 -0400

    Lab 4b

---
+++ CGrondin_Lab4B/CGrondin_Lab4B/CGrondin_Lab4B.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <fstream>
+
+
+void main() {
+
+	int d1, d2, d3, d4, number, newNumber, sum;
+
+	std::ifstream fin;
+	fin.open("C:\\Users\\008021438\\Desktop\\CGrondin_IGP\\CGrondin_Lab4B\\CGrondin_Lab4B\\Numbers.txt");
+
+	sum = 0;
+
+	while (!fin.eof()) {
+		fin >> number;
+		newNumber = number;
+		std::cout << "Original Number: " << number<< std::endl; 
+		
+		d1 = number / 1000;
+		number = number % 1000;
+		d2 = number / 100;
+		number = number % 100;
+		d3 = number / 10;
+		number = number % 10;
+		d4 = number;
+
+		sum = d1 + d2 + d3 + d4;
+
+		if (sum % 2 == 0) {
+			newNumber *= 10;
+		}
+		else {
+			newNumber *= 10;
+			newNumber += 1;
+		}
+		std::cout << "Check digit number: " << sum % 2 << std::endl;
+		std::cout << "New Number: " << newNumber << std::endl;
+		std::cout << std::endl;
+	}
+	system("exit");
+}
