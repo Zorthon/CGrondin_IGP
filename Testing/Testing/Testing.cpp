19a31c13ede6b1260ecd4891c636d521f9162949
Zorthon <104100733+Zorthon@users.noreply.github.com>
2024-04-16 09:33:30 -0400

    rted

--- Testing/Testing/Testing.cpp
+++ Testing/Testing/Testing.cpp
@@ -4,13 +4,33 @@
 #include <iomanip>
 
 void main() {
+	std::string line;
+	int count = 0;
+	char letters[50];
+	
 	std::ifstream fin;
 	fin.open("C:\\Users\\008021438\\Desktop\\CGrondin_IGP\\Testing\\Testing\\string.txt");
-	std::string line;
+	
 
 	while (!fin.eof()) {
 		getline(fin, line);
 		std::cout << line << std::endl;
 	}
 	fin.close();
+
+	int test = 0;
+	for (int i = 0; i < line.length(); i++) {
+		if (line[i] != ' ' && line[i] != '.') {
+			letters[test] = line[i];
+			test++;
+			count++;
+		}
+	}
+
+	for (int i = 0; i < count; i++) {
+		std::cout << letters[i] << std::endl;
+	}
+
+	system("pause");
+
 }
