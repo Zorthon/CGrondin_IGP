2c8f4541008cd50dc327aed9dabce70aac4d83ae
Zorthon <104100733+Zorthon@users.noreply.github.com>
2024-04-24 10:04:04 -0400

    midterm

--- CGrondin_Midterm/CGrondin_Midterm/CGrondin_Midterm.cpp
+++ CGrondin_Midterm/CGrondin_Midterm/CGrondin_Midterm.cpp
@@ -5,18 +5,52 @@
 
 int main() {
 
-	int num = 0, test1 = 0, test2 = 0;
-	int testScores[6] = { 0 };
-	std::string name, names[6] = { "" };
+	int num = 0, highestScore = 0, lowestScore = 1000, amount = 0;
+	int test1[4] = { 0 }, test2[4] = { 0 };
+	std::string highName, lowName, names[4] = { "" };
 
 	std::ifstream fin;
 	fin.open("C:\\Users\\008021438\\Desktop\\CGrondin_IGP\\CGrondin_Midterm\\Midterm1.txt");
 
 	while (!fin.eof()) {
-		fin >> name >> test1 >> test2;
-		names[num] = name;
-		std::cout << name << " " << test1 << " " << test2 << std::endl;
+		fin >> names[num] >> test1[num] >> test2[num];
+		num++;
+		amount++;
 	}
 	fin.close();
 
+	for (int i = 0; i < amount; i++) {
+		if (test1[i] > highestScore) {
+			highestScore = test1[i];
+			highName = names[i];
+		}
+		else if (test1[i] < lowestScore) {
+			lowestScore = test1[i];
+			lowName = names[i];
+		}
+	}
+
+	std::cout << "Test 1:" << std::endl;
+	std::cout << "    " << "Highest Score: " << highName << " with a " << highestScore << std::endl;
+	std::cout << "    " << "Lowest Score: " << lowName << " with a " << lowestScore << std::endl;
+	std::cout << std::endl;
+	highestScore = 0;
+	lowestScore = 1000;
+
+	for (int i = 0; i < amount; i++) {
+		if (test2[i] > highestScore) {
+			highestScore = test2[i];
+			highName = names[i];
+		}
+		else if (test2[i] < lowestScore) {
+			lowestScore = test2[i];
+			lowName = names[i];
+		}
+	}
+	
+	std::cout << "Test 2:" << std::endl;
+	std::cout << "    " << "Highest Score: " << highName << " with a " << highestScore << std::endl;
+	std::cout << "    " << "Lowest Score: " << lowName << " with a " << lowestScore << std::endl;
+
+	return 0;
 }
