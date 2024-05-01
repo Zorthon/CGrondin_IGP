16a7105b0d9c3f935a59cfc00deb0dbe2835592f
Zorthon <104100733+Zorthon@users.noreply.github.com>
2024-05-01 09:32:26 -0400

    Stuff

--- CGrondin_Lab6/CGrondin_Lab6/CGrondin_Lab6.cpp
+++ CGrondin_Lab6/CGrondin_Lab6/CGrondin_Lab6.cpp
@@ -6,7 +6,24 @@ void printArray(int arr[], int size);
 void bubbleSort(int arr[], int n);
 
 int main() {
+    
+    std::string lName[] = {" "}, FName[] = {" "};
+    int numbers[] = { 0 };
+    
+    int i, j, n;
+    for (i = 0; i < n - 1; i++)
 
+        // Last i elements are already 
+        // in place 
+        for (j = 0; j < n - i - 1; j++) {
+            if (lName[j] > lName[j + 1]) {
+                std::swap(lName[j], lName[j + 1]);
+                std::swap(FName[j], FName[j + 1]);
+                std::swap(numbers[j], numbers[j + 1]);
+                }
+                
+        }
+            
 }
 
 void bubbleSort(int arr[], int n)
