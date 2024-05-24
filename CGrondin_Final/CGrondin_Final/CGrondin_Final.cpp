27dfe7121010a722b2da9fe486355b063a891f88
Zorthon <104100733+Zorthon@users.noreply.github.com>
2024-05-24 14:53:44 -0400

    final

--- CGrondin_Final/CGrondin_Final/CGrondin_Final.cpp
+++ CGrondin_Final/CGrondin_Final/CGrondin_Final.cpp
@@ -10,38 +10,34 @@
 #include <iostream>
 
 int main() {
-	int lockers[1000], student[1000], closed = 0, open = 0;
+	int lockers[1000], student[1000], closed = 0, open = 1000;
 	//1 = open 2 = closed
-	for (int i = 1; i < 999; i++) {
+	for (int i = 1; i < 1000; i++) {
 		student[i] = i;
 		lockers[i] = 1;
 	}
 
-	for (int i = 1; i < 999; i++) {
-		for (int s = 1; s < 999; s++) {
+	for (int i = 1; i < 1000; i++) {
+		for (int s = 1; s < 1000; s++) {
 			if (student[i] % s == 0) {
 				if (lockers[i] == 1) {
 					lockers[i] = 2;
+					closed++;
+					open--;
 				}
 				else if (lockers[i] == 2) {
 					lockers[i] = 1;
+					open++;
+					closed--;
 				}
 			}
-		}
-	}
-
-	for (int i = 1; i < 999; i++) {
-		if (lockers[i] == 2) {
-			closed++;
-		}
-		else {
-			open++;
+			
 		}
 	}
 
 	std::cout << "1000 locker problem answer." << std::endl;
-	std::cout << "Closed Lockers: " << closed << std::endl;
-	std::cout << "Open Lockers: " << open << std::endl;
+	std::cout << "Open: " << open << std::endl;
+	std::cout << "Closed: " << closed << std::endl;
 	
 	return 0;
 }
